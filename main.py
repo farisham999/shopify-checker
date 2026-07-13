3f761819bb334f0b37a026446abc9ef541634a19
farisham999 <shahsham99@gmail.com>
2026-07-13 22:55:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from engine import shopify_auto_check
+import json
 import asyncio
 
 app = FastAPI()
 
-# Benarkan webpage HTML connect ke API ni
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -18,22 +18,23 @@ async def check_card(request: Request):
     data = await request.json()
     card_str = data.get("card")
     site_url = data.get("site")
-    
-    # INI DIA BACA PROXY YANG KAU HANTAR DARI INDEX.HTML
     proxy_str = data.get("proxy") 
     
     try:
-        status, message, price = await shopify_auto_check(card_str, site_url, proxy_str)
+        # engine sekarang pulangkan 4 benda: status, message, price, logs
+        status, message, price, logs = await shopify_auto_check(card_str, site_url, proxy_str)
         return {
             "status": status,
             "message": message,
-            "price": price
+            "price": price,
+            "logs": logs # KITA HANTAR LOGS NI KE WEBPAGE
         }
     except Exception as e:
         return {
             "status": "ERROR",
             "message": f"Server Error: {str(e)}",
-            "price": "-"
+            "price": "-",
+            "logs": [f"[X] Server Exception: {str(e)}"]
         }
 
 if __name__ == "__main__":
